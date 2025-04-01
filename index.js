61094860b4b5fae02d4fb175f5dac19f861f598e
omiras <oscar.miras@gmail.com>
2025-04-01 21:14:55 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Buena suerte
+
+const inputPounds = document.querySelector('#input');
+const spanResult = document.querySelector('#result');
+
+// asociamos el evento 'input'
+inputPounds.addEventListener("input", function(event){
+
+    // pounds a convertir
+    const pounds = event.target.value;
+
+    // 1 pound son 0,453592 kg
+    // Hay que multiplicar el valor de variable pounds por 0,453592
+
+    // El resultado escribirlo en el nodo #result. Podemos hacer ambas operaciones en una línea. Además redondeamos a 2 decimales
+    spanResult.textContent =  (pounds * 0.453592).toFixed(2);
+    
+
+})
